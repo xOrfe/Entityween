58ca893f53ff32372eca80c0b1e87b6c8dab5391
Batuhan Ozan Kaplan <42107185+xOrfe@users.noreply.github.com>
2021-03-15 16:03:28 +0300

    FirstPush

---
+++ EntityweenProject/Assets/Entityween/Scripts/Components/Authoring/ConvertGameobjectToEntityween.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class ConvertGameobjectToEntityween : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public string Tag;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        //Type TagType = Type.GetType(Tag);
+        //int index = TypeManager.GetTypeIndex(TagType);
+        //ComponentType mType = ComponentType.FromTypeIndex(index);
+        //dynamic instantiatedObject = Activator.CreateInstance(TagType);
+        //dstManager.AddComponentData(entity, instantiatedObject);
+    }
+}
---
+++ EntityweenProject/Assets/Entityween/Scripts/Components/Orders/ChunkFloat3ChangeOrder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Burst;
+
+namespace Entityween.Scripts
+{
+    public struct ChunkFloat3ChangeOrder : ISharedComponentData, IEquatable<ChunkFloat3ChangeOrder>
+    {
+        public int Value;
+
+        public float Time;
+
+        public float StartTime;
+
+        public float3 ChangeFactor;
+        
+        public float3 ChangeStart;
+        
+        public FunctionPointer<EaseJobs.EaseTypeDelegate> MyEaseMethod;
+        
+        public FunctionPointer<ActionJobs.ActionTypeDelegate> MyActionMethod;
+
+        public bool Equals(ChunkFloat3ChangeOrder other)
+        {
+            return Value == other.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value;
+        }
+    }
+}
+    
+
+
+
+
+
+    
