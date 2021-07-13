d7a44ec12f7c7b77364be2012af6a60394f0754f
Batuhan Ozan Kaplan <42107185+xOrfe@users.noreply.github.com>
2021-07-13 23:11:33 +0300

    lil thing

--- EntityweenProject/Assets/Entityween/Demo/Test/Spawn.cs
+++ EntityweenProject/Assets/Entityween/Demo/Test/Spawn.cs
@@ -7,38 +7,34 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 
-public class Spawn : MonoBehaviour
+namespace Entityween.Demo.Test
 {
-    public GameObject MyPrefab;
-    public int XCount = 500;
-    public int YCount = 500;
-    void Start()
+    public class Spawn : MonoBehaviour
     {
-        
-        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
-        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(MyPrefab, settings);
-        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-        for (var x = 0;x < XCount;x++)
+        public GameObject MyPrefab;
+        public int XCount = 500;
+        public int YCount = 500;
+        void Start()
         {
-            for (var y = 0;y < YCount;y++)
+            var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+            var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(MyPrefab, settings);
+            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            for (var x = 0;x < XCount;x++)
             {
-                var instance = entityManager.Instantiate(prefab);
+                for (var y = 0;y < YCount;y++)
+                {
+                    var instance = entityManager.Instantiate(prefab);
 
-                var position = transform.TransformPoint(new float3(x * 1.3f, y * 1.3f, 1));
-                entityManager.SetComponentData(instance,new Translation{ Value = position});
+                    var position = transform.TransformPoint(new float3(x * 1.3f, y * 1.3f, 1));
+                    entityManager.SetComponentData(instance,new Translation{ Value = position});
                 
-                FunctionPointer<EaseJobs.EaseTypeDelegate> myEaseMethod = BurstCompiler.CompileFunctionPointer<EaseJobs.EaseTypeDelegate>(EaseJobs.InSine);
-                FunctionPointer<ActionJobs.ActionTypeDelegate> myActionMethod = BurstCompiler.CompileFunctionPointer<ActionJobs.ActionTypeDelegate>(ActionJobs.Float3To);
-                entityManager.AddSharedComponentData(instance, new ChunkFloat3ChangeOrder {Value = Random.Range(0,10000000),MyActionMethod = myActionMethod, MyEaseMethod = myEaseMethod, Time = 300.40f, StartTime = 0.0f,ChangeStart = position, ChangeFactor = new float3(450.1F, 0, 0) });
+                    FunctionPointer<EaseJobs.EaseTypeDelegate> myEaseMethod = BurstCompiler.CompileFunctionPointer<EaseJobs.EaseTypeDelegate>(EaseJobs.InSine);
+                    FunctionPointer<ActionJobs.ActionTypeDelegate> myActionMethod = BurstCompiler.CompileFunctionPointer<ActionJobs.ActionTypeDelegate>(ActionJobs.Float3To);
+                    entityManager.AddSharedComponentData(instance, new ChunkFloat3ChangeOrder {Value = Random.Range(0,10000000),MyActionMethod = myActionMethod, MyEaseMethod = myEaseMethod, Time = 300.40f, StartTime = 0.0f,ChangeStart = position, ChangeFactor = new float3(450.1F, 0, 0) });
 
+                }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
